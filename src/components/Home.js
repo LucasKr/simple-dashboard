0d11d07df7458805ac0097b5480c02fd5ae8693b
Lucas Kruger <lucas.kr1996@gmail.com>
2018-03-14 17:36:51 -0300

    Deploy

--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Index = ({ app }) => (
-    <div>
-        Y're at Home page, {app.name}!
-    </div>
-);
-
-export default Index;
--- src/reducers/app.js
+++
@@ -1,7 +0,0 @@
-const INITIAL_STATE = {
-    name: "Kruger"
-};
-
-export default function(state = INITIAL_STATE, action) {
-    return state;
-}
