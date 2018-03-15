6d68b44ce879b4509c28326bc715f3ad9676bd6d
Lucas Kruger <lucas.kr1996@gmail.com>
2018-03-15 19:18:04 -0300

    Charts for diseases

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,75 @@
 import React from 'react';
 
-const mapPropToDashboardData = (diseases) => {
-    let dashboardData = {
-        labels: [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007],
-        series: []
-    }
-    dashboardData.series = diseases.map(element => {
-        let result = new Array(dashboardData.labels.length);
-        element.data.forEach(i => {
-            let indexOfLabel = dashboardData.labels.indexOf(i.year);
-            if(indexOfLabel !== -1) {
-                result[indexOfLabel] = i.people;
-            }
-        });
-        return { 
-            name: element.name,
-            data: result
-        };
-    });
-    return <h1>Hello World</h1>
+import { Line, Bar } from 'react-chartjs';
+
+
+import '../styles/dashboard-area.css';
+
+const dashboardOptions = {
+    responsive: true,
+    scaleShowGridLines: true,
+    scaleGridLineColor: 'rgba(0,0,0,0.05)',
+    scaleGridLineWidth: 1,
+    scaleShowHorizontalLines: true,
+    scaleShowVerticalLines: true,
+    bezierCurve: true,
+    bezierCurveTension: 0.4,
+    pointDot: true,
+    pointDotRadius: 4,
+    pointDotStrokeWidth: 1,
+    pointHitDetectionRadius: 20,
+    datasetStroke: true,
+    datasetStrokeWidth: 2,
+    datasetFill: true,
+    legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
 }
 
+const mapPropToDashboardData = (labels, diseases) => {
+    return {
+        labels: labels,
+        datasets : diseases.map((element, index) => {
+            let result = new Array(labels.length);
+            element.data.forEach(i => {
+                let indexOfLabel = labels.indexOf(i.year);
+                if(indexOfLabel !== -1) {
+                    result[indexOfLabel] = i.people;
+                }
+            });
+            return { 
+                label: element.name,
+                data: result,
+                fillColor: "rgba(255, 64, "+index+",0.5)",
+                strokeColor: "rgba(255, 64,"+index+",0.5)",
+                pointColor: "rgba(255, 64,"+index+",0.5)",
+                pointStrokeColor: "rgba(255, 64,0,0.5)",
+                pointHighlightFill: "rgba(255, 64,"+index+",0.5)",
+                pointHighlightStroke: "rgba(255, 64, "+index+",0.5)",
+            };
+        })
+    };
+}
+
+const RenderDashboardLine = ({labels, diseases}) => {
+    return (
+        <Line data={mapPropToDashboardData(labels, diseases)} options={dashboardOptions} />
+    );
+}
+
+const RenderDashboardBar = ({labels, diseases}) => {
+    return (
+        <Bar data={mapPropToDashboardData(labels, diseases)} options={dashboardOptions} />
+    );
+}
+
+
 const Dashboard = ({ dashboard }) => (
-    <div>
-    { mapPropToDashboardData(dashboard.diseases) }
+    <div className="DashboardArea">
+        <div className="DashboardItem">
+            <RenderDashboardLine labels={[2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]} diseases={dashboard.diseases} />
+        </div>
+        <div className="DashboardItem">
+            <RenderDashboardBar labels={[2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007]} diseases={dashboard.diseases} />
+        </div>
     </div>
 );
 
